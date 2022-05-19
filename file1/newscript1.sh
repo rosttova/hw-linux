4a35ea8f110710c83cf739875cf019d5f3f543af
rosttova <104725369+rosttova@users.noreply.github.com>
2022-05-19 18:58:42 +0300

    Add files via upload

---
+++ file1/newscript1.sh
@@ -0,0 +1,17 @@
+#!/bin?bash
+cd file1;
+
+mkdir test1 test2 test3;
+
+cd test1;
+
+touch f1.txt f2.txt f3.txt f4.json f5.json;
+
+mkdir test4 test5 test6;
+
+mkdir -l;
+
+cd test1 ;
+
+echo "Успех :)";
+
